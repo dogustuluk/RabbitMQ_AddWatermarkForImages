42a5e1668d33bb8473a3ce6bb7f0f16ac23bd451
dogustuluk <dogus.tuluk@gmail.com>
2022-05-06 16:09:52 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQ.publisher
+{
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://nxdranwu:n_3nr-xZlXx0NoCWuFP05gTqZfp7_hwK@sparrow.rmq.cloudamqp.com/nxdranwu");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = headers;
+            properties.Persistent = true; //persistent'i true'ya set ederek mesajları kalıcı hale getirmiş olduk. Bunu yapabilmek için bir properties oluşturmamız gereklidir.
+
+            var product = new Product { Id = 1, Name = "Conklin Herringbone", Price = 1500, Stock = 5 };
+
+            var productJsonString = JsonSerializer.Serialize(product);
+
+
+            channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
+
+            Console.WriteLine("Mesaj Gönderilmiştir");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace RabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://nxdranwu:n_3nr-xZlXx0NoCWuFP05gTqZfp7_hwK@sparrow.rmq.cloudamqp.com/nxdranwu");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            headers.Add("x-match", "all"); //x-match yapısı
+            //all >>>> ifade "all" ise publish edilen ile subscribe'daki headerlardaki key-value birebir aynısı olmalıdır.
+            //any >>>> ifade "any" ise publishteki ile subscribe'taki header'lardan herhangi biri farklı olsa dahi mesaj gönderilir.
+
+            channel.QueueBind(queueName, "header-exchange", string.Empty, headers);
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("loglar dinleniyor");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Product product = JsonSerializer.Deserialize<Product>(message);
+
+                Thread.Sleep(1500);
+
+                Console.WriteLine($"Gelen Mesaj: {product.Id}-{product.Name}-{product.Price}-{product.Stock}"  /*"Gelen mesaj: " + message*/);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            Console.ReadLine();
+        }
+    }
+}
