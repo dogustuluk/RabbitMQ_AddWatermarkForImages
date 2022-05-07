7d04b43c1edbc9acb9a854df0991ecc25dd8826e
dogustuluk <dogus.tuluk@gmail.com>
2022-05-07 15:44:48 +0300

    background services > ImageWatermarkProcessBackgroundService class created

---
+++ RabbitMQWeb.Watermark/BackgroundServices/ImageWatermarkProcessBackgroundService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQWeb.Watermark.BackgroundServices
+{
+    public class ImageWatermarkProcessBackgroundService : BackgroundService
+    {
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            return base.StartAsync(cancellationToken);
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
