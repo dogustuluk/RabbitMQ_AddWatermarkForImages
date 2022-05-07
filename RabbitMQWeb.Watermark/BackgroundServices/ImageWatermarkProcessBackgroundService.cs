3076bd85e5be409d8f4143a2efd8efb138351db2
dogustuluk <dogus.tuluk@gmail.com>
2022-05-07 15:53:07 +0300

    ImageWatermarkProcessBackgroundService >> startAsync method created

--- RabbitMQWeb.Watermark/BackgroundServices/ImageWatermarkProcessBackgroundService.cs
+++ RabbitMQWeb.Watermark/BackgroundServices/ImageWatermarkProcessBackgroundService.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQWeb.Watermark.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +12,22 @@ namespace RabbitMQWeb.Watermark.BackgroundServices
 {
     public class ImageWatermarkProcessBackgroundService : BackgroundService
     {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+        private readonly ILogger _logger;
+        private IModel _channel;
+
+        public ImageWatermarkProcessBackgroundService(RabbitMQClientService rabbitMQClientService, ILogger<ImageWatermarkProcessBackgroundService> logger)
+        {
+            _rabbitMQClientService = rabbitMQClientService;
+            _logger = logger;
+        }
+
         public override Task StartAsync(CancellationToken cancellationToken)
         {
+            _channel = _rabbitMQClientService.Connect();
+            _channel.BasicQos(0, 1, false);
+
+
             return base.StartAsync(cancellationToken);
         }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
